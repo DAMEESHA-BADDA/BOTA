fcfbb45ed2bd1d50ca6f15c514f9cdea1e1bd3f9
DAMEESHA-BADDA <dameeshawesabadda@proton.me>
2025-03-15 16:54:14 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳aZxlkIpK#L5aseoVOAqk3ENLZ6tdeOyvd9w-LpXT8TiWe0_tPvjs' : process.env.SESSION_ID,
+PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
+SESSION_NAME: process.env.PORT === undefined ? "nadeen" : process.env.SESSION_NAME,
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
+};
